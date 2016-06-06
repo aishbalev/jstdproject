739e0c9dada31559d39561d3dc3adc01ade7d3b9
aishbalev <aishbalev@gmail.com>
2016-06-06 13:51:47 -0500

    Add files via upload

--- pagefind.js
+++ pagefind.js
@@ -2,98 +2,235 @@ function mysubmit() {
 	document.getElementById("outbox").innerHTML = 'yoooou';
 }
 
-var mapDiv;
+var dataLayer;
 var map;
+var page2char = ["contents", "Tsura", "Tsura and Seraph", "Tsura, Seraph, and Alex", "Alex", "Alex", "Alex and Ruth", "Ruth", "Ruth and Elise", "Elise and Marko", "Elise and Marko", "Elise and Marko", "Elise and Marko", "Elise and Marko", "Marko and Kizzy", "Kizzy", "Kizzy",]
+var page2loc = new Array(17)
+page2loc[1] = "an unidentified train station"
+page2loc[2] = "Meet-up near Friedrichshain Park"
+page2loc[3] = "Tsura and Seraph walk to Seraph and Wolf's appartment. Alex is headed toward a small shop."
+page2loc[4] = "the photographer's shop"
+page2loc[5] = "the photographer's shop"
+page2loc[6] = "Alex is in the photographer's shop. Ruth is at the cathedral."
+page2loc[7] = "St. Hedwigs Cathedral"
+page2loc[8] = "Ruth is at St. Hedwig's Cathedral.  Elise is on her way to Ruth's from Girls' League."
+page2loc[9] = "Hackescher Market"
+page2loc[10] = "Hackescher Market"
+page2loc[11] = "Hackescher Market"
+page2loc[12] = "Hackescher Market"
+page2loc[13] = "Elise heads to Ruth's.  Marko is in Hackescher Market"
+page2loc[14] = "Marko is heading to Duer's.  Kizzy is at Duer's."
+page2loc[15] = "at Duer's"
+page2loc[16] = "Kizzy leaves Duer's heading to Seraph and Wolf's"
 
 
 function initMap() {
 	var map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat:52.52, lng: 13.405},
-		zoom: 9
+		zoom: 11
 	});
-	var dataLayer = new google.maps.KmlLayer({
-		src: 'TRAIN LOCATIONS.kml',
-		map: map
-	});
-/*	var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 11,
-    center: {lat: 41.876, lng: -87.624}
-  });
+ 
+}
 
-  var ctaLayer = new google.maps.KmlLayer({
-    url: 'http://googlemaps.github.io/js-v2-samples/ggeoxml/cta.kml',
-    map: map
-  });*/
+function kinit() {
+	//reinitialize the map
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat:52.52, lng: 13.405},
+		zoom: 11
+	});
+	//add the Kizzy layer
+	var dataLayer = new google.maps.KmlLayer({
+		url: 'http://aishbalev.github.io/jstdproject/Kizzy.kml',
+		map: map
+	}); 
 }
 
 function Enter() {
 	if (event.keyCode == 13) {
-		document.getElementById("searchbutton").click();
+		pageSearch();
 	}
 }
 
-// This example adds a search box to a map, using the Google Place Autocomplete
-// feature. People can enter geographical searches. The search box will return a
-// pick list containing a mix of places and predicted search terms.
-
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
 function pageSearch() {
 	//grab the input
-	var input = document.getElementById('inbox');
-  // Create the search box and link it to the UI element.
-  var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+	var input = document.getElementById('inbox').value;
+	//reinitialize the map
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat:52.52, lng: 13.405},
+		zoom: 11
+	});	
 
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
-  });
+	var markers = [];
+	
+	var marker1 = new google.maps.Marker({
+		position: {lat:52.526, lng: 13.424},
+		map: map,
+		title: 'Barnimstraﬂe'
+	});
 
-  var markers = [];
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener('places_changed', function() {
-    var places = searchBox.getPlaces();
+    var marker2 = new google.maps.Marker({
+		position: {lat: 52.489, lng: 13.339},
+		map: map,
+		title: 'Bavarian Quarter - Bayerisches Viertel'
+	}); 
+	
+	var marker3 = new google.maps.Marker({
+		position: {lat: 52.503, lng: 13.381},
+		map: map,
+		title: 'Berlin Anhalter Bahnhof (train station)'
+	});
 
-    if (places.length == 0) {
-      return;
-    }
+    var marker4 = new google.maps.Marker({
+		position: {lat: 52.527, lng: 13.378},
+		map: map,
+		title: 'CharitÈ Hospital'
+	});
+	
+	var marker5 = new google.maps.Marker({
+		position: {lat: 52.491, lng: 13.40832},
+		map: map,
+		title: 'Fontanepromenade'
+	});
 
-    // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(null);
-    });
-    markers = [];
+    var marker6 = new google.maps.Marker({
+		position: {lat: 52.515802, lng: 13.45423},
+		map: map,
+		title: 'Friedrichshain'
+	});	
+	
+	var marker7 = new google.maps.Marker({
+		position: {lat: 52.525579, lng: 13.398986},
+		map: map,
+		title: 'Groﬂe Hamburger Straﬂe'
+	});
 
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
+    var marker8 = new google.maps.Marker({
+		position: {lat: 52.492065, lng: 13.284936},
+		map: map,
+		title: 'Grunewald (freight yard)'
+	});	
+	
+	var marker9 = new google.maps.Marker({
+		position: {lat: 52.5231, lng: 13.402951},
+		map: map,
+		title: 'Hackescher Markt'
+	});
 
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
+    var marker10 = new google.maps.Marker({
+		position: {lat: 52.512842, lng: 13.430593},
+		map: map,
+		title: 'Lange Straﬂe'
+	});	
+	
+	var marker11 = new google.maps.Marker({
+		position: {lat: 52.503674, lng: 13.362489},
+		map: map,
+		title: 'L¸tzowstraﬂe'
+	});
 
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
-  });
+    var marker12 = new google.maps.Marker({
+		position: {lat: 52.527947, lng: 13.426742},
+		map: map,
+		title: 'M‰rchenbrunnen'
+	});	
+	
+	var marker13 = new google.maps.Marker({
+		position: {lat: 52.55, lng: 13.55},
+		map: map,
+		title: 'Marzahn'
+	});
+
+    var marker14 = new google.maps.Marker({
+		position: {lat: 52.529485, lng: 13.343204},
+		map: map,
+		title: 'Moabit'
+	});
+  	
+	var marker21 = new google.maps.Marker({
+		position: {lat: 52.498054, lng: 13.337326},
+		map: map,
+		title: 'Motzstraﬂe and Kalckreuthstraﬂe'
+	});
+
+    var marker15 = new google.maps.Marker({
+		position: {lat: 52.532389, lng: 13.34102},
+		map: map,
+		title: 'Putlitz Street Bahnhof (train station)'
+	});	
+	
+	var marker16 = new google.maps.Marker({
+		position: {lat: 52.521535, lng: 13.404927},
+		map: map,
+		title: 'Rosenstraﬂe'
+	});
+
+    var marker17 = new google.maps.Marker({
+		position: {lat: 52.540861, lng: 13.258015},
+		map: map,
+		title: 'Siemensstadt'
+	});	
+	
+	var marker18 = new google.maps.Marker({
+		position: {lat: 52.538413, lng: 13.413577},
+		map: map,
+		title: 'Sredzkistraﬂe, Schultheiss Brewery'
+	});
+
+    var marker19 = new google.maps.Marker({
+		position: {lat: 52.516011, lng: 13.394161},
+		map: map,
+		title: 'St. Hedwigs Kathedrale'
+	});	
+	
+	var marker20 = new google.maps.Marker({
+		position: {lat: 52.5106187, lng: 13.3689022},
+		map: map,
+		title: 'Tiergartenstraﬂe 4'
+	});
+	/*var dataLayer = new google.maps.KmlLayer({
+		url: 'http://aishbalev.github.io/jstdproject/PageMap.kml',
+		map: map
+	});*/
+	if (input.length != 0) {
+		//clear away the markers
+			marker1.setMap(null);
+			marker2.setMap(null);
+			marker3.setMap(null);
+			marker4.setMap(null);
+			marker5.setMap(null);
+			marker6.setMap(null);
+			marker7.setMap(null);
+			marker8.setMap(null);
+			marker9.setMap(null);
+			marker10.setMap(null);
+			marker11.setMap(null);
+			marker12.setMap(null);
+			marker13.setMap(null);
+			marker14.setMap(null);
+			marker15.setMap(null);
+			marker16.setMap(null);
+			marker17.setMap(null);
+			marker18.setMap(null);
+			marker19.setMap(null);
+			marker20.setMap(null);
+			marker21.setMap(null);
+		//dataLayer.setMap(null);
+		//describe the characters' locations
+		var outwords = "CHARACTERS: "
+		var pagechars = page2char[input];
+		var moreout = "LOCATION: "
+		var pagelocs = page2loc[input];
+		document.getElementById("names").innerHTML = outwords.concat(pagechars);
+		document.getElementById("locs").innerHTML = moreout.concat(pagelocs);
+		//put in the marker
+		if (input == 6 || input == 7 || input == 8){
+			marker7.setMap(map);
+		}
+		if (input == 9 || input == 10 || input == 11 || input == 12 || input == 13 || input == 14){
+			marker9.setMap(map);
+		}
+		if (input == 14 || input == 15 || input == 16){
+			marker14.setMap(map);
+		}
+	}
 }
